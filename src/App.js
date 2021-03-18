d4d11ab27e6835437558a81a96c42c9fb81898f0
imane-benchaiba <imyben.bi@gmail.com>
2021-03-18 10:53:33 +0100

    final result

--- src/App.js
+++ src/App.js
@@ -3,22 +3,59 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      <form>      
+        <div class="form-row">
+          <div class="form-group col-md-6">
+            <label for="inputEmail4">Email</label>
+            <input
+              type="email"
+              class="form-control"
+              id="inputEmail4"
+              placeholder="Email"
+            />
+          </div>
+          <div class="form-group col-md-6">
+            <label for="inputPassword4">Mot de passe</label>
+            <input
+              type="password"
+              class="form-control"
+              id="inputPassword4"
+              placeholder="Mot de passe"
+            />
+          </div>
+        </div>
+        <div class="form-group">
+          <label for="inputAddress">Addresse</label>
+          <input
+            type="text"
+            class="form-control"
+            id="inputAddress"
+            placeholder="Cité..."
+          />
+        </div>
+        <div class="form-row">
+          <div class="form-group col-md-6">
+            <label for="inputCity">Commune</label>
+            <input type="text" class="form-control" id="inputCity" />
+          </div>
+          <div class="form-group col-md-4">
+            <label for="inputState">Wilaya</label>
+            <select id="inputState" class="form-control">
+              <option selected>Alger</option>
+              <option>Boumerdes</option>
+              <option>Blida</option>
+            </select>
+          </div>
+          <div class="form-group col-md-2">
+            <label for="inputZip">Code</label>
+            <input type="text" class="form-control" id="inputZip" />
+          </div>
+        </div>
+        <button type="submit" class="btn btn-primary">S'identifier</button>
+      </form>
     </div>
+
   );
 }
 
